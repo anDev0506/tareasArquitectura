50ee3bab68da9f7e6241b7df746a08245524cad1
anDev0506 <217121874+anDev0506@users.noreply.github.com>
2026-02-23 16:02:11 -0400

    Se refactorizaron los mensajes de salida y se mejoro el codigo de Paridad Instantaneo.asm

--- Practicas de laboratorio/Practica1/src/Paridad Instantaneo.asm
+++ Practicas de laboratorio/Practica1/src/Paridad Instantaneo.asm
@@ -1,49 +1,37 @@
 .data
-	text1: .asciiz "Ingrese numero :  "
-	text2: .asciiz "Su numero es par.\n"
-	text3: .asciiz "Su numero es impar.\n"
-	text4: .asciiz "Numero Invalido!"
+ 	text1: .asciiz "Ingrese numero : "
+ 	text2: .asciiz "Su numero es par.\n"
+ 	text3: .asciiz "Su numero es impar.\n"
+ 	text4: .asciiz "Numero Invalido!\n"
 .text
-	main:
-	
-		li $v0, 4
-		la $a0, text1
+ 	main:
+
+ 		li $v0, 4
+ 		la $a0, text1
 		syscall
-	
-		li $v0, 5
-		syscall
-	
-		move $a0, $v0
-		jal paridad
-		move $s0, $v0
-	
-		li $v0, 4
-		beq $s0, -1, error
-		beq $s0, 0, impar
-	par:
-	
-		la $a0, text2
-		j terminar
-	
-	impar:
-		la $a0, text3
-		j terminar
-	error:
-		la $a0, text4
-		j terminar
-	
-	
+
+ 		li $v0, 5
+ 		syscall
+
+ 		move $a0, $v0
+ 		jal paridad
+ 		move $s0, $v0
+
+ 		li $v0, 4
+ 		beq $s0, 1, impar
+ 	par:
+ 		la $a0, text2
+ 		j terminar
+
+ 	impar:
+  		la $a0, text3
+ 		j terminar
+
 	terminar:
 		syscall
-		li $v0, 10
-		syscall
-	
-	paridad:
-		li $v0, -1
-		bltz $a0, return
-	
-		li $t0, 1
-		andi $t1, $a0, 1
-		sub $v0, $t0, $t1
-	return:
-		jr $ra
+ 		li $v0, 10
+ 		syscall
+
+ 	paridad:
+ 		andi $v0 , $a0 , 1
+ 		jr $ra
